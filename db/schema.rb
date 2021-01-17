34dd44ebc232910a8ef763d1620b14b000e14960
Saryn Mooney <smooney15@my.whitworth.edu>
2021-01-16 18:59:59 -0700

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_17_002116) do
+ActiveRecord::Schema.define(version: 2021_01_17_014304) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,14 @@ ActiveRecord::Schema.define(version: 2021_01_17_002116) do
 
   create_table "images", force: :cascade do |t|
     t.string "url"
+    t.string "title"
+    t.string "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "photos", force: :cascade do |t|
+    t.string "source"
     t.string "title"
     t.string "description"
     t.datetime "created_at", precision: 6, null: false
