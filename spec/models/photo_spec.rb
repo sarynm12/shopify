0e40087927d39225e826621df37ecfb11b2b9992
Saryn Mooney <smooney15@my.whitworth.edu>
2021-01-16 21:07:42 -0700

    create user table with spec folders

--- spec/models/photo_spec.rb
+++ spec/models/photo_spec.rb
@@ -1,6 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Photo, type: :model do
+  describe 'relationships' do
+    it {should belong_to :user}
+  end
   describe 'validations' do
     it {should validate_presence_of :source}
     it {should validate_presence_of :title}
